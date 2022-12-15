bd1328593a1583091dffa18b6340b591df1f1aa6
harshavardhan2112 <108981384+harshavardhan2112@users.noreply.github.com>
2022-12-16 00:28:00 +0530

    Add files via upload

--- UCCFINAL/src/App.js
+++ UCCFINAL/src/App.js
@@ -5,21 +5,26 @@ import { Home } from "./components/Pages/Home";
 import { Events } from "./components/Pages/Events";
 import { Team } from "./components/Pages/Team";
 import { Contact } from "./components/Pages/Contact";
+import {Resources} from "./components/Pages/Resources";
 import Form from './components/Pages/form'
+import Adminlogin from "./components/Pages/Adminlogin";
+import viewdata from "./components/Pages/viewdata";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-
         <div className="pages">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/Events" component={Events} />
             <Route path="/Team" component={Team} />
             <Route path="/contact" component={Contact} />
-            <Route path='/form' component={Form}/>
+            <Route path='/form' component={Form}/> 
+            <Route path = '/Resources' component ={Resources}/>
+            <Route path="/adminlogin" component={Adminlogin}/>
+            <Route path="/viewdata" component={viewdata}/>
           </Switch>
         </div>
       </Router>
